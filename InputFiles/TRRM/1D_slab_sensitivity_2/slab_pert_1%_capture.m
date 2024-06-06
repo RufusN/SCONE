7f14d19873973b94905adc9df7be41018830dddc
RufusN <rufus@neame.org>
2024-06-06 16:52:58 +0100

    slab inputs/outputs

---
+++ InputFiles/TRRM/1D_slab_sensitivity_2/slab_pert_1%_capture.m
@@ -0,0 +1,12 @@
+seed = 1259489730;
+pop = 1000;
+Inactive_Cycles = 400;
+Active_Cycles = 300;
+Total_CPU_Time = 5.01946E+01;
+Total_Transport_Time = 8.12953E+00;
+Time_Per_Integration = 1.58419E-07;
+Clock_Time = 8.38340E+00;
+sensitivity = -1.72085E-03;
+keff_keff = [ 1.00000E+00,4.63319E-10];
+Adjoint_keff_Adjoint_keff = [ 1.00000E+00,4.63319E-10];
+
