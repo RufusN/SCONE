00f5a43757ad65299af14a09bd930edf26c7d2fd
RufusN <rufus@neame.org>
2024-06-06 15:09:01 +0100

    inputs

---
+++ InputFiles/TRRM/1D_slab_anisotropy/slab_pert_unpert.m
@@ -0,0 +1,12 @@
+seed = -763885711;
+pop = 1000;
+Inactive_Cycles = 400;
+Active_Cycles = 300;
+Total_CPU_Time = 4.43743E+01;
+Total_Transport_Time = 7.29659E+00;
+Time_Per_Integration = 1.42186E-07;
+Clock_Time = 7.52331E+00;
+sensitivity = -5.14583E-03;
+keff_keff = [ 1.63145E+00,1.05398E-05];
+Adjoint_keff_Adjoint_keff = [ 1.63144E+00,1.52130E-05];
+
