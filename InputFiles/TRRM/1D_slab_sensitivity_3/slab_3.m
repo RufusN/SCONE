9dcf89bb92c85188f96f1d4b8041b213f94d1cb2
RufusN <rufus@neame.org>
2024-06-12 15:35:32 +0100

    sorted files

---
+++ InputFiles/TRRM/1D_slab_sensitivity_3/slab_3.m
@@ -0,0 +1,14 @@
+seed = 1452798954;
+pop = 1000;
+Inactive_Cycles = 400;
+Active_Cycles = 300;
+Total_CPU_Time = 4.40134E+01;
+Total_Transport_Time = 7.26740E+00;
+Time_Per_Integration = 9.61390E-08;
+Clock_Time = 7.43971E+00;
+keff_keff = [ 9.78590E-01,8.87930E-06];
+Adjoint_keff_Adjoint_keff = [ 9.78592E-01,9.25674E-06];
+delta_keff_delta_keff = [ 0.00000E+00,0.00000E+00];
+pert_keff_pert_keff = [ 9.78590E-01,9.78590E-01];
+sensitivity_sensitivity = [ 0.00000E+00,0.00000E+00];
+
