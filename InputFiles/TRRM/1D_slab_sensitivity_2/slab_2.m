b240c5932af8637c33e8cea693568d07209e82a9
RufusN <rufus@neame.org>
2024-07-02 00:04:26 +0100

    added pertubation field

--- InputFiles/TRRM/1D_slab_sensitivity_2/slab_2.m
+++ InputFiles/TRRM/1D_slab_sensitivity_2/slab_2.m
@@ -1,14 +1,14 @@
-seed = 824554453;
+seed = 690344368;
 pop = 1000;
 Inactive_Cycles = 400;
 Active_Cycles = 300;
-Total_CPU_Time = 3.75025E+00;
-Total_Transport_Time = 2.85837E+00;
-Time_Per_Integration = 5.67120E-08;
-Clock_Time = 2.87095E+00;
+Total_CPU_Time = 2.37918E+01;
+Total_Transport_Time = 6.21719E+00;
+Time_Per_Integration = 1.23363E-07;
+Clock_Time = 6.33139E+00;
 keff_keff = [ 1.00000E+00,4.63319E-10];
 Adjoint_keff_Adjoint_keff = [ 1.00000E+00,4.63319E-10];
-delta_keff_delta_keff = [ 4.16667E-03,5.35315E-12];
+delta_keff_delta_keff = [ 4.16667E-03,1.44831E-11];
 pert_keff_pert_keff = [ 1.00417E+00,9.95833E-01];
-sensitivity_sensitivity = [ 4.16667E-01,1.59207E-09];
+sensitivity_sensitivity = [ 4.16667E-01,1.75831E-09];
 
--- InputFiles/TRRM/1D_slab_sensitivity_3/slab_3.m
+++ InputFiles/TRRM/1D_slab_sensitivity_3/slab_3.m
@@ -1,14 +1,14 @@
-seed = -2121487330;
+seed = -895018583;
 pop = 1000;
 Inactive_Cycles = 400;
 Active_Cycles = 300;
-Total_CPU_Time = 3.23168E+01;
-Total_Transport_Time = 6.50409E+00;
-Time_Per_Integration = 8.60371E-08;
-Clock_Time = 6.64893E+00;
+Total_CPU_Time = 3.69512E+01;
+Total_Transport_Time = 6.87760E+00;
+Time_Per_Integration = 9.09756E-08;
+Clock_Time = 7.07332E+00;
 keff_keff = [ 9.99997E-01,1.96482E-07];
 Adjoint_keff_Adjoint_keff = [ 9.99997E-01,1.93035E-07];
-delta_keff_delta_keff = [ 0.00000E+00,0.00000E+00];
-pert_keff_pert_keff = [ 9.99997E-01,9.99997E-01];
-sensitivity_sensitivity = [ 0.00000E+00,0.00000E+00];
+delta_keff_delta_keff = [ -2.17707E-03,8.32746E-12];
+pert_keff_pert_keff = [ 9.97820E-01,1.00217E+00];
+sensitivity_sensitivity = [ -2.17708E-01,1.52134E-09];
 
