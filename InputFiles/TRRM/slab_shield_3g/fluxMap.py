2579b82e39e1528daf1e99c19fd907988412443c
RufusN <rufus@neame.org>
2024-11-20 01:28:39 +0000

    pin problem and python scripts

--- InputFiles/TRRM/slab_shield_3g/fluxMap.py
+++ InputFiles/TRRM/slab_shield_3g/fluxMap.py
@@ -4,14 +4,16 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load the JSON data from a file
-file_name = '/Users/ru/SCONE/InputFiles/TRRM/slab_shield_3g/pin_test.json'  # Replace with your actual file path
+file_name = '/Users/ru/SCONE/InputFiles/TRRM/slab_shield_3g/slab_shield_3g_planes.json'  # Replace with your actual file path
 
 try:
     with open(file_name, 'r') as f:
         data = json.load(f)
 
     # Access the 'responseRate' field
-    response_rates = data['responseRate']['responseRate']
+    response_rates = data['fluxMap']['fluxMap']
+
+    # response_rates = response_rates[:][:][1]
 
     # Extract the first elements from the nested lists and organize into a 36x20 grid
     heatmap_data = np.array([[pair[0] for pair in outer_list] for outer_list in response_rates])
